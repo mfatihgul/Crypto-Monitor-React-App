a8b75eeeefbee304d9b6ceebab3a1634731d1fc2
mfatihgul <mfg7ix@gmail.com>
2021-12-18 16:39:42 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [cryptos, setCryptos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => setCryptos(res.data))
+      .catch((error) => console.log({ error }));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1> Crypto Monitor </h1>
+      </div>
+      <table>
+        <tr>
+          <td> Name </td>
+          <td> Symbol </td>
+          <td> Current Price </td>
+          <td> %Change(24h)</td>
+        </tr>
+        {cryptos.map((crypto) => {
+          return (
+            <tr>
+              <td> {crypto.name} </td>
+              <td style={{ textTransform: "uppercase" }}> {crypto.symbol} </td>
+              <td> {crypto.current_price} </td>
+              <td> {crypto.price_change_percentage_24h} % </td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
-
-export default App;
